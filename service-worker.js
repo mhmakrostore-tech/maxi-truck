d0880cc299b61bcb9b762941a67045ee2b400b6e
mhmakrostore-tech <mh.makrostore@gmail.com>
2026-08-12 11:42:05 -0400

    Add files via upload

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='maxi-truck-v11';
-const ASSETS=['./','index.html','style.css','app.js','manifest.json','icon.svg','firebase-app.js'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
